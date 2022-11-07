917b37658e9bd2bb81b531b70dfc299209d98001
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2022-11-07 16:13:49 +0100

    component edit

--- src/pages/TodoEdit.jsx
+++ src/pages/TodoEdit.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { useState } from 'react'
 
 function TodoEdit() {
+
+  const [ titleInput, setTitleInput ] = useState("")
+  const [ descriptionInput, setDescriptionInput ] = useState("")
+  const [ isUrgentInput, setIsUrgentInput ] = useState(false)
+
+  const titleChange = (event) => setTitleInput(event.target.value)
+  const descriptionChange = (event) => setDescriptionInput(event.target.value)
+  const isUrgentChange = (event) => setIsUrgentInput(event.target.checked)
+
   return (
-    <div>TodoEdit</div>
+    <div>
+      
+      <h3>Formulario Editar</h3>
+
+      <form>
+
+        <label htmlFor="title">Titulo</label>
+        <input type="text" name="title" value={titleInput} onChange={titleChange}/>
+        <br />
+        <label htmlFor="description">Descripción</label>
+        <input type="text" name="description" value={descriptionInput} onChange={descriptionChange}/>
+        <br />
+        <label htmlFor="isUrgent">Es Urgente</label>
+        <input type="checkbox" name="isUrgent" value={isUrgentInput} onChange={isUrgentChange}/>
+        <br />
+        <button>Editar ToDo</button>
+      </form>
+
+    </div>
   )
 }
 
