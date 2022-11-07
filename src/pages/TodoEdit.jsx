846fcbc265b8d3a80009b577c37a754c013b6f8e
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2022-11-07 16:35:55 +0100

    CRUD done

--- src/pages/TodoEdit.jsx
+++ src/pages/TodoEdit.jsx
@@ -1,15 +1,60 @@
 import React from 'react'
+import { useEffect } from 'react'
 import { useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { getTodoDetailsService, updateTodoService } from '../services/todo.services'
 
 function TodoEdit() {
+
+  const { todoId } = useParams()
+  const navigate = useNavigate()
 
   const [ titleInput, setTitleInput ] = useState("")
   const [ descriptionInput, setDescriptionInput ] = useState("")
   const [ isUrgentInput, setIsUrgentInput ] = useState(false)
 
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const getData = async () => {
+    try {
+      const response = await getTodoDetailsService(todoId)
+      console.log(response)
+  
+      // que debo hacer para actualizar los campos con response?
+      setTitleInput(response.data.title)
+      setDescriptionInput(response.data.description)
+      setIsUrgentInput(response.data.isUrgent)
+    } catch (error) {
+      navigate("/error")
+    }
+  }
+
   const titleChange = (event) => setTitleInput(event.target.value)
   const descriptionChange = (event) => setDescriptionInput(event.target.value)
   const isUrgentChange = (event) => setIsUrgentInput(event.target.checked)
+
+  const handleUpdate = async (event) => {
+    event.preventDefault()
+    try {
+      
+      // recopilamos los valor a actualizar
+      const updatedTodo = {
+        title: titleInput,
+        description: descriptionInput,
+        isUrgent: isUrgentInput
+      }
+      // llamamos al servicio de update pasando el Id y la data a actualizar
+      await updateTodoService(todoId, updatedTodo)
+
+      // redireccionar
+      navigate("/todos")
+
+    } catch (error) {
+      navigate("/error")
+    }
+  }
 
   return (
     <div>
@@ -25,9 +70,9 @@ function TodoEdit() {
         <input type="text" name="description" value={descriptionInput} onChange={descriptionChange}/>
         <br />
         <label htmlFor="isUrgent">Es Urgente</label>
-        <input type="checkbox" name="isUrgent" value={isUrgentInput} onChange={isUrgentChange}/>
+        <input type="checkbox" name="isUrgent" checked={isUrgentInput} onChange={isUrgentChange}/>
         <br />
-        <button>Editar ToDo</button>
+        <button onClick={handleUpdate}>Editar ToDo</button>
       </form>
 
     </div>
