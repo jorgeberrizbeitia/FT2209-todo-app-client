2b3538255428443810d780561088067abea0437e
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2022-11-10 13:06:54 +0100

    added server url

--- src/services/config.services.js
+++ src/services/config.services.js
@@ -4,7 +4,7 @@ import axios from "axios"
 
 // crear el archivo base de servicios
 const service = axios.create({
-  baseURL: process.env.SERVER_URL
+  baseURL: process.env.REACT_APP_SERVER_URL
 })
 
 
